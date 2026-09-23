04f96079d3492b64e66b7aa6d8a835e9679c5ec7
Erozah <mickael.plantard@gmail.com>
2026-09-23 16:05:59 +0200

    Feat: Documentation swagger

--- src/main/java/fr/games/square/controller/UserController.java
+++ src/main/java/fr/games/square/controller/UserController.java
@@ -3,12 +3,15 @@ package fr.games.square.controller;
 import fr.games.square.dto.UserCreationDto;
 import fr.games.square.dto.UserDto;
 import fr.games.square.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
+@Tag(name = "Utilisateurs", description = "Gestion des utilisateurs")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,24 +22,28 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Créer un utilisateur")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserDto create(@RequestBody UserCreationDto dto) {
         return userService.createUser(dto);
     }
 
+    @Operation(summary = "Obtenir un utilisateur")
     @GetMapping("/{id}")
     public UserDto get(@PathVariable UUID id) {
         return userService.getUserById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Supprimer un utilisateur")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID id) {
         userService.deleteUser(id);
     }
 
+    @Operation(summary = "Vérifier si un identifiant utilisateur est valide")
     @GetMapping("/{id}/valid")
     public boolean isValid(@PathVariable UUID id) {
         if (!userService.isUserValid(id)) {
